de25be326cdad1560e31daca866fc6e88d758f3a
polamjag <s@polamjag.info>
2013-12-02 19:42:23 +0900

    refactored symlinker.sh and created setup.sh, integrated cloning .emacs.d

---
+++ setup.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/sh
+
+# setup.sh - setting up dotfiles
+# create symbolic link of dotfiles in which same with this script in your home directory
+# NOTICE: this script processes only files whose name begin with . (dot)
+
+echo -e "Creating symbolic link of dotfiles ..."
+
+shdir="$(cd $(dirname $0) && pwd)"
+
+cd ${shdir}
+
+for filepath in ${shdir}/.*
+do
+    if [ \( -f $filepath -o -d $filepath \) -a $filepath != "${shdir}/." -a $filepath != "${shdir}/.." -a $filepath != "${shdir}/.git" ] ; then
+				echo "creating link: ${filepath} -> ${HOME}"
+				ln -s ${filepath} ${HOME}
+    fi
+done
+
+function setup_emacsd() {
+		echo "Cloning .emacs.d ..."
+		cd $HOME
+		git clone https://github.com/polamjag/.emacs.d.git
+		pwd
+}
+
+if [ ! -e ~/.emacs.d ] ; then
+		echo -n "Would you setup .emacs.d with github.com:polamjag/.emacs.d.git? (y/n) "
+		read answer
+		case $answer in
+				y)
+						setup_emacsd
+						;;
+				Y)
+						setup_emacsd
+						;;
+				yes)
+						setup_emacsd
+						;;
+				*)
+						echo "Aborted cloning .emacs.d"
+						;;
+		esac
+fi
+
+echo "Finished setup"
