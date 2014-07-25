81b2c770d5f7465785ea707ce995c9315c13e7c8
polamjag <s@polamjag.info>
2014-07-26 01:00:47 +0900

    fix indent

--- setup.sh
+++ setup.sh
@@ -4,66 +4,66 @@ shdir="$(cd $(dirname $0) && pwd)"
 cd ${shdir}
 
 setup_dot() {
-    echo "  Setting up dotfiles ..."
-    for filepath in ${shdir}/.* ; do
-        if [ \( -f $filepath -o -d $filepath \) -a $filepath != "${shdir}/." -a $filepath != "${shdir}/.." -a $filepath != "${shdir}/.git" -a $filepath != "${shdir}/.gitconfig" -a $filepath != "${shdir}/.gitignore" -a $filepath != "${shdir}/.gitmodules"  -a $filepath != "${shdir}/.zshenv.exam" ] ; then
-            echo "    Creating link: ${filepath} -> ${HOME}"
-            ln -s ${filepath} ${HOME}
-        fi
-    done
-    if [ ! -f $HOME/.zshenv ] ; then
-        cp $shdir/.zshenv.exam $HOME/.zshenv
+  echo " Setting up dotfiles ..."
+  for filepath in ${shdir}/.* ; do
+    if [ \( -f $filepath -o -d $filepath \) -a $filepath != "${shdir}/." -a $filepath != "${shdir}/.." -a $filepath != "${shdir}/.git" -a $filepath != "${shdir}/.gitconfig" -a $filepath != "${shdir}/.gitignore" -a $filepath != "${shdir}/.gitmodules" -a $filepath != "${shdir}/.zshenv.exam" ] ; then
+      echo "  Creating link: ${filepath} -> ${HOME}"
+      ln -s ${filepath} ${HOME}
     fi
+  done
+  if [ ! -f $HOME/.zshenv ] ; then
+    cp $shdir/.zshenv.exam $HOME/.zshenv
+  fi
 }
 setup_git() {
-    echo "  Setting up .gitconfig ..."
-    ln -s ${shdir}/.gitconfig $HOME
+  echo " Setting up .gitconfig ..."
+  ln -s ${shdir}/.gitconfig $HOME
 }
 setup_bin () {
-    echo "  Setting up ~/bin ..."
-    if [ ! -d ${HOME}/bin ] ; then
-        mkdir ${HOME}/bin
-    fi
-    for filepath in ${shdir}/bin/* ; do
-        echo "    Creating link: ${filepath} -> ${HOME}/bin/"
-        ln -s ${filepath} ${HOME}/bin/
-    done
+  echo " Setting up ~/bin ..."
+  if [ ! -d ${HOME}/bin ] ; then
+    mkdir ${HOME}/bin
+  fi
+  for filepath in ${shdir}/bin/* ; do
+    echo "  Creating link: ${filepath} -> ${HOME}/bin/"
+    ln -s ${filepath} ${HOME}/bin/
+  done
 }
 setup_binx () {
-    echo "  Setting up ~/bin_x ..."
-    if [ ! -d ${HOME}/bin ] ; then
-        mkdir ${HOME}/bin
-    fi
-    for filepath in ${shdir}/bin_x/* ; do
-        echo "    Creating link: ${filepath} -> ${HOME}/bin/"
-        ln -s ${filepath} ${HOME}/bin/
-    done
+  echo " Setting up ~/bin_x ..."
+  if [ ! -d ${HOME}/bin ] ; then
+    mkdir ${HOME}/bin
+  fi
+  for filepath in ${shdir}/bin_x/* ; do
+    echo "  Creating link: ${filepath} -> ${HOME}/bin/"
+    ln -s ${filepath} ${HOME}/bin/
+  done
 }
 setup_vim () {
-    echo "  Setting up ~/.vim/ ..."
-    cd $shdir
-    git submodule init
-    git submodule update
-    cd $shdir/.vim/bundle/vimproc
-    make
+  echo " Setting up ~/.vim/ ..."
+  cd $shdir
+  git submodule init
+  git submodule update
+  cd $shdir/.vim/bundle/vimproc
+  make
 }
 
 if [ $# -eq 1 -a "$1" = "--usage" ] ; then
-    echo "$0 [--usage] [--force <args>]"
-    echo "args: dot, git, bin, binx, vim"
-    echo
-    echo "e.g.: \`$0 --force dot git\`"
-    exit 0
+  echo "$0 [--usage] [--force <args>]"
+  echo "args: dot, git, bin, binx, vim"
+  echo
+  echo "e.g.: \`$0 --force dot git\`"
+  exit 0
 fi
 
 if [ $# -gt 1 -a "$1" = "--force" ] ; then
-    echo "Running in force mode"
+  echo "Running in force mode"
+  shift
+  while [ $# -gt 0 ] ; do
+    setup_$1
     shift
-    while [ $# -gt 0 ] ; do
-        setup_$1
-        shift
-    done
-    exit 0
+  done
+  exit 0
 fi
 
 # interactive mode
@@ -73,89 +73,89 @@ echo -e "Creating symbolic link of dotfiles ..."
 echo -n "Setup dotfiles? (y/n) "
 read answer
 case $answer in
-    y)
-        setup_dot
-        ;;
-    Y)
-        setup_dot
-        ;;
-    yes)
-        setup_dot
-        ;;
-    *)
-        echo
-        ;;
+  y)
+    setup_dot
+    ;;
+  Y)
+    setup_dot
+    ;;
+  yes)
+    setup_dot
+    ;;
+  *)
+    echo
+    ;;
 esac
 
 # .gitconfig
 if [ ! -e $HOME/.gitconfig ] ; then
-    echo -n "Use .gitconfig? (y/n) "
+  echo -n "Use .gitconfig? (y/n) "
 		read $answer
-    case $answer in
-        y)
-            setup_git
-            ;;
-        Y)
-            setup_git
-            ;;
-        yes)
-            setup_git
-            ;;
-        *)
-            ;;
-    esac
+  case $answer in
+    y)
+      setup_git
+      ;;
+    Y)
+      setup_git
+      ;;
+    yes)
+      setup_git
+      ;;
+    *)
+      ;;
+  esac
 fi
 
 # ~/bin
 echo -n "Copy shell scripts **for console** into ~/bin? (y/n) "
 read answer
 case $answer in
-    y)
-        setup_bin
-        ;;
-    Y)
-        setup_bin
-        ;;
-    yes)
-        setup_bin
-        ;;
-    *)
-        echo
-        ;;
+  y)
+    setup_bin
+    ;;
+  Y)
+    setup_bin
+    ;;
+  yes)
+    setup_bin
+    ;;
+  *)
+    echo
+    ;;
 esac
 
 echo -n "Copy shell scripts **for X Desktop Environment** into ~/bin? (y/n) "
 read answer
 case $answer in
-    y)
-        setup_binx
-        ;;
-    Y)
-        setup_binx
-        ;;
-    yes)
-        setup_binx
-        ;;
-    *)
-        echo
-        ;;
+  y)
+    setup_binx
+    ;;
+  Y)
+    setup_binx
+    ;;
+  yes)
+    setup_binx
+    ;;
+  *)
+    echo
+    ;;
 esac
 
 echo -n "Execute some commands to initialize vim environment? (y/n) "
 read answer
 case $answer in
-    y)
-        setup_vim
-        ;;
-    Y)
-        setup_vim
-        ;;
-    yes)
-        setup_vim
-        ;;
-    *)
-        echo
-        ;;
+  y)
+    setup_vim
+    ;;
+  Y)
+    setup_vim
+    ;;
+  yes)
+    setup_vim
+    ;;
+  *)
+    echo
+    ;;
 esac
 
 echo "Finished setup"
