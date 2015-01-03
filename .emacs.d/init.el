23365e52385abf352f0364fce1cf5ba58f20cead
polamjag <s@polamjag.info>
2015-01-03 22:58:39 +0900

    * .emacs.d/init.el: add keybinds for anzu

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -471,6 +471,8 @@
  '(anzu-mode-lighter "")
  '(anzu-deactivate-region t)
  '(anzu-search-threshold 1000))
+(global-set-key (kbd "M-%") 'anzu-query-replace)
+(global-set-key (kbd "C-M-%") 'anzu-query-replace-regexp)
 
 
 ;;;; eshell
