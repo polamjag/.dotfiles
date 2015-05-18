527b471286e3c0d85942744b2bc99d16ee372f0f
polamjag <s@polamjag.info>
2015-05-19 06:12:25 +0900

    update

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,7 +2,7 @@
         :plugins [[lein-kibit "0.0.8"]
                   [lein-pprint "1.1.2"]
                   [lein-spell "0.1.0"]
-                  [cider/cider-nrepl "0.7.0"]
+                  [cider/cider-nrepl "0.8.2"]
                   ]
        :dependencies [[slamhound "1.5.5"]]}
  }
