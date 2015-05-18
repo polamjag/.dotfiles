c9fca17650a5fd542038b63f0435ac110ab7f29a
polamjag <s@polamjag.info>
2015-05-19 06:18:34 +0900

    add org.clojure/tools.nrepl "0.2.7" as deps

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,14 @@
-{:user {
-        :plugins [[lein-kibit "0.0.8"]
-                  [lein-pprint "1.1.2"]
-                  [lein-spell "0.1.0"]
-                  [cider/cider-nrepl "0.8.2"]
-                  ]
-       :dependencies [[slamhound "1.5.5"]]}
+{:user
+ {
+  :plugins [
+            [lein-kibit "0.0.8"]
+            [lein-pprint "1.1.2"]
+            [lein-spell "0.1.0"]
+            [cider/cider-nrepl "0.8.2"]
+            ]
+  :dependencies [
+                 [slamhound "1.5.5"]
+                 [org.clojure/tools.nrepl "0.2.7"]
+                 ]
+  }
  }
