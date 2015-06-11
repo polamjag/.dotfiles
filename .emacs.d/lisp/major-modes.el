29963130609b90aba606928279758a5e2a0d8b10
polamjag <s@polamjag.info>
2015-06-12 01:17:11 +0900

    add some configs for go

--- .emacs.d/lisp/major-modes.el
+++ .emacs.d/lisp/major-modes.el
@@ -91,6 +91,18 @@
             (c-turn-on-eldoc-mode)))
 
 
+;;; go
+(eval-after-load "go-mode" '(progn (require 'go-autocomplete)))
+(add-hook 'go-mode-hook
+          '(lambda()
+             (add-hook 'before-save-hook 'gofmt-before-save)
+             (setq indent-tabs-mode t)
+             (local-set-key (kbd "M-.") 'godef-jump)
+             (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
+             (local-set-key (kbd "C-c i") 'go-goto-imports)
+             (local-set-key (kbd "C-c d") 'godoc))
+
+
 ;;; qml-mode
 (add-to-list 'auto-mode-alist '("\\.qml$" . qml-mode))
 (setq qml-mode-indent-offset 2)
