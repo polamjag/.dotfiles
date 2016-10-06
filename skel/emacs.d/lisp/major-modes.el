3eba07569820dc1e45510a0320121639ad72a0f9
polamjag <s@polamjag.info>
2016-10-06 15:35:02 +0900

    open .tt template in web-mode

--- skel/emacs.d/lisp/major-modes.el
+++ skel/emacs.d/lisp/major-modes.el
@@ -28,6 +28,7 @@
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.json\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tt\\'" . web-mode))
 (add-hook
  'web-mode-hook
  '(lambda ()
